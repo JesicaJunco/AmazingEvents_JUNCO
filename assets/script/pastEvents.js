8727b52901b7922e98ab06ca13bb7adb1b587bd4
Jesica <jesibjunco@hotmail.com>
2023-03-16 07:34:51 -0300

    modificacion en pastEvent y upcomingEvent

--- assets/script/pastEvents.js
+++ assets/script/pastEvents.js
@@ -1,5 +1,5 @@
 import data from "./amazing.js";
-import {pintarCard, createCheckboxes,filtroCombinado,filtrarPorTexto,filtrarCategoria } from "./functions.js"; 
+import { pintarCard, createCheckboxes, filtroCombinado, filtrarPorTexto, filtrarCategoria } from "./functions.js";
 //capturo los contenedores de card, checkbox, y el input del buscador
 const contenedor = document.getElementById("contenedorCard");
 const contenedorCheck = document.getElementById("checkContainer");
